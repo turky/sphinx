501593c7267d9f242ca071eab2bd8f58da10a4e3
Akihiro Takizawa <akihiro.takizawa@gmail.com>
2020-08-11 19:26:39 +0900

    fix latex package in ja. pxjahyper doesn't need with lualatex.

--- sphinx/builders/latex/__init__.py
+++ sphinx/builders/latex/__init__.py
@@ -461,7 +461,8 @@ def validate_latex_theme_options(app: Sphinx, config: Config) -> None:
 def install_pakcages_for_ja(app: Sphinx) -> None:
     """Install packages for Japanese."""
     if app.config.language == 'ja':
-        app.add_latex_package('pxjahyper', after_hyperref=True)
+        if app.config.latex_engine != 'lualatex':
+            app.add_latex_package('pxjahyper', after_hyperref=True)
 
 
 def default_latex_engine(config: Config) -> str:
